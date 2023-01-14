7d02a6982951b7581bb075f844f6bc039bb0b089
wang-bin <wbsecg1@gmail.com>
2023-01-14 17:42:22 +0800

    add metal debayer. only rgb48le works
    slightly faster then r3d decoder

--- Debayer.cpp
+++ Debayer.cpp
@@ -3,11 +3,20 @@
  * r3d plugin for libmdk
  */
 #include "Debayer.h"
+#include "R3DSDK.h"
 
 MDK_NS_BEGIN
 
+#if  (__APPLE__ + 0)
+GpuDebayer::Ptr CreateMTLDebayer();
+#endif
 GpuDebayer::Ptr GpuDebayer::create(int type)
 {
+#if  (__APPLE__ + 0)
+    if (type & OPTION_RED_METAL) {
+        return CreateMTLDebayer();
+    }
+#endif // (__APPLE__ + 0)
     return nullptr;
 }
 
